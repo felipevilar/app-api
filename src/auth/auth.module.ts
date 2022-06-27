465b0fcf1cfcf984acfe5cb786d6e990d45153e6
Felipe Vilar <felipvilar@gmail.com>
2022-06-26 22:59:11 -0400

    Added local auth strategy

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { UserModule } from 'src/user/user.module';
+import { AuthService } from './auth.service';
+import { LocalStrategy } from './local.strategy';
+
+@Module({
+  imports: [
+    UserModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: {
+          expiresIn: configService.get('JWT_EXPIRES_IN'),
+        },
+      }),
+    }),
+  ],
+  providers: [AuthService, LocalStrategy],
+  exports: [AuthService],
+})
+export class AuthModule {}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly userService: UserService,
+    private jwtService: JwtService,
+  ) {}
+
+  async validateUser(email: string, password: string): Promise<any> {
+    const findUser = await this.userService.findByEmail(email);
+    if (findUser && bcrypt.compareSync(password, findUser.password)) {
+      const user = {
+        _id: findUser._id,
+        email: findUser.email,
+        role: findUser.role,
+      };
+      return user;
+    }
+    return null;
+  }
+
+  async login(user: any) {
+    return {
+      access_token: this.jwtService.sign(user),
+    };
+  }
+}
